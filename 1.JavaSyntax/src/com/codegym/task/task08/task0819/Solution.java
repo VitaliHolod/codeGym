c8c17d11f3f56cf90bad9083b87a9d6e666d0487
Vitali Holod <holod.vitali@gmail.com>
2019-01-15 12:30:33 +0300

    Add new files

---
+++ 1.JavaSyntax/src/com/codegym/task/task08/task0819/Solution.java
@@ -0,0 +1,40 @@
+package com.codegym.task.task08.task0819;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Set of cats
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+        Set<Cat> copy = cats;
+
+        for (Cat c : copy) {
+            cats.remove(c);
+            break;
+        }
+
+        printCats(cats);
+    }
+
+    public static Set<Cat> createCats() {
+        Set<Cat> cats = new HashSet<>();
+        cats.add(new Cat());
+        cats.add(new Cat());
+        cats.add(new Cat());
+        return cats;
+    }
+
+    public static void printCats(Set<Cat> cats) {
+        for (Cat c : cats) {
+            System.out.println(c);
+        }
+    }
+
+    public static class Cat {
+    }
+}
---
+++ 1.JavaSyntax/src/com/codegym/task/task08/task0820/Solution.java
@@ -0,0 +1,67 @@
+package com.codegym.task.task08.task0820;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Animal set
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+        Set<Dog> dogs = createDogs();
+
+        Set<Object> pets = join(cats, dogs);
+        printPets(pets);
+
+        removeCats(pets, cats);
+        printPets(pets);
+    }
+
+    public static Set<Cat> createCats() {
+        HashSet<Cat> result = new HashSet<Cat>();
+
+        result.add(new Cat());
+        result.add(new Cat());
+        result.add(new Cat());
+        result.add(new Cat());
+
+        return result;
+    }
+
+    public static Set<Dog> createDogs() {
+
+        HashSet<Dog> dogs = new HashSet<>();
+
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+
+        return dogs;
+    }
+
+    public static Set<Object> join(Set<Cat> cats, Set<Dog> dogs) {
+        HashSet<Object> objects = new HashSet<>();
+
+        objects.addAll(cats);
+        objects.addAll(dogs);
+
+        return objects;
+    }
+
+    public static void removeCats(Set<Object> pets, Set<Cat> cats) {
+        pets.removeAll(cats);
+    }
+
+    public static void printPets(Set<Object> pets) {
+       pets.forEach(System.out::println);
+    }
+
+    public static class Cat {
+    }
+
+    public static class Dog {
+    }
+}
