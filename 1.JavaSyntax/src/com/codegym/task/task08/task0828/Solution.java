eef4718204d96313d51d2216f2dcf9bc74b66c98
Vitali Holod <holod.vitali@gmail.com>
2019-01-18 13:36:08 +0300

    Add new files

--- 1.JavaSyntax/src/com/codegym/task/task08/task0828/Solution.java
+++ 1.JavaSyntax/src/com/codegym/task/task08/task0828/Solution.java
@@ -1,3 +1,4 @@
+
 package com.codegym.task.task08.task0828;
 
 import java.io.BufferedReader;
@@ -7,39 +8,19 @@ import java.util.*;
 
 /* 
 Month number
-
 */
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-
-        Scanner scanner = new Scanner(System.in);
-        String numberMonth = scanner.nextLine();
-
-        Map<String, String> map = new HashMap<>();
-
-        map.put("1", "January");
-        map.put("2", "February");
-        map.put("3", "March");
-        map.put("4", "April");
-        map.put("5", "May");
-        map.put("6", "June");
-        map.put("7", "July");
-        map.put("8", "August");
-        map.put("9", "September");
-        map.put("10", "October");
-        map.put("11", "November");
-        map.put("12", "December");
-
-        String s = map.values().stream().filter(v -> v.equals(numberMonth)).toString();
-
-        System.out.println(s);
-
-
-        for (Map.Entry<String, String> entry: map.entrySet()){
-            if (map.equals(numberMonth)) {
-                System.out.println(entry.getValue() + " is month "+ entry.getKey());
-            }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String month = br.readLine();
+        String[] s = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(s));
+        int i = 1;
+        for(String str : list) {
+            if(month.equals(str))
+                System.out.println(str + " is month " + i);
+            i++;
         }
     }
 }
